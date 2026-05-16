439b17c121838d1bf29e7456dadd02c0e1e8d5fa
wearevelocite <wearevelocite@gmail.com>
2026-05-16 07:07:52 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,7 @@ import Hero from '@/components/Hero'
 import Navbar from '@/components/Navbar'
 import Cursor from '@/components/Cursor'
 import Blobs from '@/components/Blobs'
-
-import Clients from '@/components/Clients'
-import Work from '@/components/Work'
-import Team from '@/components/Team'
-import Mission from '@/components/Mission'
-import CTA from '@/components/CTA'
-import Footer from '@/components/Footer'
-
+import { Clients, Work, Team, Mission, CTA, Footer } from '@/components/sections'
 export default function Home() {
   return (
     <>
