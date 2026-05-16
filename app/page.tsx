67329e97a077169cf381503c04a2c312f5f68a54
wearevelocite <wearevelocite@gmail.com>
2026-05-16 07:15:32 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from '@/components/Navbar'
 import Cursor from '@/components/Cursor'
 import Blobs from '@/components/Blobs'
 import { Clients, Work, Team, Mission, CTA, Footer } from '@/components/sections'
+
 export default function Home() {
   return (
     <>
